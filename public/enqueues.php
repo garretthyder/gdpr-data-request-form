5e67859269c28094e14bf9b97a33ea10d5f136e9
Garrett Hyder <garrett.m.hyder@gmail.com>
2020-02-21 14:02:11 -0800

    Merge the enqueue calls into one function
    No need to have two functions attached to the same action.

--- public/enqueues.php
+++ public/enqueues.php
@@ -10,14 +10,10 @@
  * @subpackage gdpr-data-request-form/public
  * @prefix     gdrf_
  */
-
-add_action( 'wp_enqueue_scripts', 'enqueue_styles_gdrf_public' );
-function enqueue_styles_gdrf_public() {
-	wp_register_style( 'gdrf-public-styles', plugin_dir_url( __FILE__ ) . 'css/public.css', array(), '', 'all' );
-}
-
 add_action( 'wp_enqueue_scripts', 'enqueue_scripts_gdrf_public' );
 function enqueue_scripts_gdrf_public() {
+	wp_register_style( 'gdrf-public-styles', plugin_dir_url( __FILE__ ) . 'css/public.css', array(), '', 'all' );
+	
 	wp_register_script( 'gdrf-public-scripts', plugin_dir_url( __FILE__ ) . 'js/gdrf-public.js', array( 'jquery' ), '', false );
 	$translations = array(
 		'gdrf_ajax_url' => esc_url( admin_url( 'admin-ajax.php' ) ),
